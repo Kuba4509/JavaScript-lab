cb3d6198806a0d9f0f8820f8ff1bb6400f1c3f59
Kuba4509 <121046205+Kuba4509@users.noreply.github.com>
2024-03-13 17:09:23 +0100

    Add files via upload

--- JS/lab2/script.js
+++ JS/lab2/script.js
@@ -1,9 +1,9 @@
 // tworenie img w js
-const img1 =  document.createElement('img')
+// const img1 =  document.createElement('img')
 // lub
-const img2 = new Image()
+// const img2 = new Image()
 
-img1.src=""
+// img1.src=""
 
 //setTimeout(), setInterval()
 // setTimeout(
@@ -14,15 +14,36 @@ img1.src=""
 //     },2_000
 // )
 
-let positionX = 0
-const anim = setInterval(
-    ()=>{
-        const box = document.querySelector('#slider-inner')
-        box.style.transform = `translate(-` + positionX + `px,0px)`
-        positionX++
-    },16)
+// let positionX = 0
+// const anim = setInterval(
+//     ()=>{
+//         const box = document.querySelector('#slider-inner')
+//         box.style.transform = `translate(-` + positionX + `px,0px)`
+//         positionX++
+//     },16)
 
-    setTimeout(()=>{
-        clearInterval(anim)
-    }, 6_000)
+//     setTimeout(()=>{
+//         clearInterval(anim)
+//     }, 6_000)
+
+let position = 0;
+Sliders();
+
+function Sliders(){
+    let i;
+    let slide = document.getElementsByClassName("slider-inner");
+    for (i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";      
+    }
+
+    function zmiana(n){
+        Sliders()
+    }
+
+    position++;
+    if (position > slide.length){position = 1}
+
+    slide[position-1].style.display = "block";
+    setTimeout(Sliders, 2000);
+}
     
